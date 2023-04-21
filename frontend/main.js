58291a6f3f08b554734800f4b77c906554f5c76d
Sebastian Post <sebastianpost@protonmail.com>
2023-04-21 17:54:47 +0200

    Edited login system

--- frontend/main.js
+++ frontend/main.js
@@ -1,17 +1,18 @@
 import initEditor from "./js/editor.js";
+const loggedIn = localStorage.getItem("loggedIn");
 
-const login = document.querySelector(".login");
-login.addEventListener("click", clickLogin)
+if (loggedIn === "true") {
+  initEditor()
+} else {
+  const login = document.querySelector(".login");
+  login.addEventListener("click", clickLogin)
 
+    function clickLogin(){
+      
+      localStorage.setItem("loggedIn", "true")
+      console.log("Loggades in")
 
-
-
-function clickLogin(){
-
-
-  localStorage.setItem("loggedIn", "true")
-    console.log("Loggades in")
-
-initEditor();
-
+    initEditor();
+  }
 }
+
